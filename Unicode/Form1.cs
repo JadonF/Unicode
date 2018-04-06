d31e3c0e4536b8f6ad84f91aa2d6f1df3dcdcd1b
JadonF <jadon.fournier@ocsbstudent.ca>
2018-04-06 11:06:18 -0400

    Initial commit

---
+++ Unicode/Form1.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by: Jadon Fournier
+ * Created on: 06-Apr-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day # - Unicode
+ * This program changes unicode to a letter
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unicode
+{
+    public partial class frmUnicode : Form
+    {
+
+        public frmUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            //Define Variables
+            int counter;
+            string container;
+
+            //clear list
+            this.lstUnicode.Items.Clear();
+
+            for (counter = 65; counter <= 90; counter++)
+            {
+                container = Char.ConvertFromUtf32(counter);
+                lstUnicode.Items.Add(container + "-->" + counter);
+            }
+        }
+    }
+}
+
